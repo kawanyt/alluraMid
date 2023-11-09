a11189975cadb0b6b2b2e041c4e71f4382f55efb
kawanyt <145021162+kawanyt@users.noreply.github.com>
2023-11-09 09:35:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play('');
+function tocaSom(){
+document.querySelector().play('');
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom ();
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
  
-while()[
-listaDeTeclas[0].onclick = tocaSomPom;
-]
+    while(contador < 9){ 
+for (contador=0;contador<listaDeTeclas.length; contador++){
+    const instrumento=listaDeTeclas[contador];
+
+    const tecla=listaDeTeclas[contador];
+
+    const idAudio=`#som_${instrumento}`;
+listaDeTeclas[contador].onclick = function()  
+tocaSom(idAudio);
+
+    tocaSom('#som_tecla_clap');
+    };
+listaDeTeclas[contador].clsslist[1];
+tocaSom('#som_tecla_clap');
+    }
+    (evento.code==='enter')
+  
+        
+    
+
+
