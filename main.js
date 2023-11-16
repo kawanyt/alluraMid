707d69ddeb87752bf194c1504cbeab218b1dbf2f
kawanyt <145021162+kawanyt@users.noreply.github.com>
2023-11-16 08:56:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ document.querySelector().play('');
 document.querySelector('.tecla_pom').onclick = tocaSomPom ();
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
- 
+ if(Elemento!=null && elemnto.localname==='audio'){
+    Elemento.play();
+ }
     while(contador < 9){ 
 for (contador=0;contador<listaDeTeclas.length; contador++){
     const instrumento=listaDeTeclas[contador];
@@ -14,6 +16,18 @@ for (contador=0;contador<listaDeTeclas.length; contador++){
     const idAudio=`#som_${instrumento}`;
 listaDeTeclas[contador].onclick = function()  
 tocaSom(idAudio);
+};
+
+tecla.onkeydown=function(evento){
+    if(evento.code==='space'|| evento.code==='enter'){
+    tecla.clsslist.add('ativa');
+}
+tecla.onkeyup=function(){
+    tecla.clsslist.remove('ativa');
+}
+
+
+
 
     tocaSom('#som_tecla_clap');
     };
